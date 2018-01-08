510fcfc814cfd7124b2cb2947b8b911b87dadf26
walkingalchemy <walkingalchemy@gmail.com>
2018-01-08 16:10:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  return Math.abs(location - 42)
+}
+
+
+function calculateVertical(low, high) {
+  return (high - low) * 264
+}
+
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination)
+  switch (true) {
+    case distance <= 400:
+      return 0
+    case distance > 2500:
+      return 'cannot travel that far'
+    case distance > 2000:
+      return 25
+    case distance > 400:
+      return distance * .02
+  }
+}
